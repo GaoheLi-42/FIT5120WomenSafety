5e0e33785dcd6d93c77274dee58a7797f5b833e5
Gaohe Li <63134205+GaoheLi-42@users.noreply.github.com>
2021-04-22 10:18:23 +1000

    SECOND COMMIT

---
+++ ai1wm-backups/security_level_sensor.php
@@ -0,0 +1,429 @@
+?>
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:v="urn:schemas-microsoft-com:vml">
+<head>
+
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+<meta content="width=device-width" name="viewport"/>
+
+<meta content="IE=edge" http-equiv="X-UA-Compatible"/>
+
+
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css"/>
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css"/>
+
+<style type="text/css">
+	
+		body {
+			margin: 0;
+			padding: 0;
+			
+		}
+
+		table,
+		td,
+		tr {
+			vertical-align: top;
+			border-collapse: collapse;
+		}
+
+		* {
+			line-height: inherit;
+		}
+
+		a[x-apple-data-detectors=true] {
+			color: inherit !important;
+			text-decoration: none !important;
+		}
+	</style>
+<style id="media-query" type="text/css">
+		@media (max-width: 620px) {
+
+			.block-grid,
+			.col {
+				min-width: 320px !important;
+				max-width: 100% !important;
+				display: block !important;
+			}
+
+			.block-grid {
+				width: 100% !important;
+			}
+
+			.col {
+				width: 100% !important;
+			}
+
+			.col_cont {
+				margin: 0 auto;
+			}
+
+			img.fullwidth,
+			img.fullwidthOnMobile {
+				max-width: 100% !important;
+			}
+
+			.no-stack .col {
+				min-width: 0 !important;
+				display: table-cell !important;
+			}
+
+			.no-stack.two-up .col {
+				width: 50% !important;
+			}
+
+			.no-stack .col.num2 {
+				width: 16.6% !important;
+			}
+
+			.no-stack .col.num3 {
+				width: 25% !important;
+			}
+
+			.no-stack .col.num4 {
+				width: 33% !important;
+			}
+
+			.no-stack .col.num5 {
+				width: 41.6% !important;
+			}
+
+			.no-stack .col.num6 {
+				width: 50% !important;
+			}
+
+			.no-stack .col.num7 {
+				width: 58.3% !important;
+			}
+
+			.no-stack .col.num8 {
+				width: 66.6% !important;
+			}
+
+			.no-stack .col.num9 {
+				width: 75% !important;
+			}
+
+			.no-stack .col.num10 {
+				width: 83.3% !important;
+			}
+
+			.video-block {
+				max-width: none !important;
+			}
+
+			.mobile_hide {
+				min-height: 0px;
+				max-height: 0px;
+				max-width: 0px;
+				display: none;
+				overflow: hidden;
+				font-size: 0px;
+			}
+
+			.desktop_hide {
+				display: block !important;
+				max-height: none !important;
+			}
+		}
+	</style>
+<style id="icon-media-query" type="text/css">
+		@media (max-width: 620px) {
+			.icons-inner {
+				text-align: center;
+			}
+
+			.icons-inner td {
+				margin: 0 auto;
+			}
+		}
+	</style>
+
+
+</head>
+<body class="clean-body" style="margin: 0; padding: 0; -webkit-text-size-adjust: 100%; background-color: transparent;">
+<table  cellpadding="0" cellspacing="0" class="nl-container" role="presentation" style="table-layout: fixed; vertical-align: top; min-width: 320px; border-spacing: 0; border-collapse: collapse; mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color:transparent; width: 100%;border: none;" valign="top" width="100%">
+<tbody>
+<tr style="vertical-align: top;border: none;" valign="top">
+<td style="word-break: break-word; vertical-align: top;border: none;" valign="top">
+<div style="background-color:transparent;">
+
+</div>
+</div>
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+</div>
+<div style="background-color:transparent;">
+
+</div>
+</div>
+
+</div>
+</div>
+
+<div align="center" class="button-container" style="padding-top:10px;padding-right:10px;padding-bottom:10px;padding-left:10px;">
+
+</div>
+
+</div>
+
+</div>
+</div>
+
+
+</div>
+</div>
+
+</div>
+</div>
+
+<div align="center" class="button-container" style="padding-top:10px;padding-right:10px;padding-bottom:10px;padding-left:10px;">
+
+</div>
+
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+</div>
+
+
+</div>
+</div>
+
+</div>
+</div>
+
+<div align="center" class="button-container" style="padding-top:10px;padding-right:10px;padding-bottom:10px;padding-left:10px;">
+
+</div>
+
+</div>
+
+</div>
+</div>
+
+
+</div>
+</div>
+
+
+</div>
+</div>
+
+<div align="center" class="button-container" style="padding-top:10px;padding-right:10px;padding-bottom:10px;padding-left:10px;">
+
+</div>
+
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+</div>
+<div style="background-color:transparent;">
+<div class="block-grid" style="min-width: 320px; max-width: 600px; overflow-wrap: break-word; word-wrap: break-word; word-break: break-word; Margin: 0 auto; background-color: transparent;">
+<div style="border-collapse: collapse;display: table;width: 100%;background-color:transparent;background-position:top left;background-repeat:no-repeat">
+<div class="col num12" style="min-width: 320px; max-width: 600px; display: table-cell; vertical-align: top; width: 600px;">
+<div class="col_cont" style="width:100% !important;">
+
+<div style="border-top:0px solid transparent; border-left:0px solid transparent; border-bottom:0px solid transparent; border-right:0px solid transparent; padding-top:35px; padding-bottom:40px; padding-right: 0px; padding-left: 0px;">
+<div style="color:#393d47;font-family:Montserrat, Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Tahoma, sans-serif;line-height:1.2;padding-top:10px;padding-right:10px;padding-bottom:10px;padding-left:10px;">
+<div class="txtTinyMce-wrapper" style="line-height: 1.2; font-size: 12px; color: #393d47; font-family: Montserrat, Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Tahoma, sans-serif; mso-line-height-alt: 14px;">
+<p style="font-size: 34px; line-height: 1.2; word-break: break-word; text-align: center; mso-line-height-alt: 41px; margin: 0;"><span style="font-size: 34px;"><strong>Sensors Details</strong></span></p>
+</div>
+</div>
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+</div>
+
+<center>
+<div style="width: 40%;" >
+    <input id="searchInput" "type="text" placeholder="Enter your Location">
+      <button style="-webkit-text-size-adjust: none; text-decoration: none; display: inline-block; color: #ffffff; background-color: #ffae01; border-radius: 0px; -webkit-border-radius: 0px; -moz-border-radius: 0px; width: auto; width: auto; border-top: 0px solid #8a3b8f; border-right: 0px solid #8a3b8f; border-bottom: 0px solid #8a3b8f; border-left: 0px solid #8a3b8f; padding-top: 5px; padding-bottom: 5px; font-family: Montserrat, Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Tahoma, sans-serif; text-align: center; mso-border-alt: none; word-break: keep-all;" onclick="search()"> 
+		Search
+		<script>
+function search(){
+				var searchInput = document.getElementById("searchInput").value;
+				if(searchInput){
+					window.location.href=`?search=${searchInput}`;
+				}
+			}
+		</script>
+      </button>
+ </div>
+</center>
+
+
+
+<?php
+	$search = $_GET['search'];
+	$servername = "localhost";
+	$username = "root";
+	$password = "yumin0817CCXL+";
+	$dbname = "bitnami_wordpress";
+
+	// connect
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+		echo "aaa";
+		die("connection failed: " . $conn->connect_error);
+	}
+	$result_per_page=5;
+	$sql = "SELECT * FROM security1";
+	if ($search){
+		$sql = $sql." where sensor_desc like '%".$_GET['search']."%'";
+	}
+	$result = $conn->query($sql);
+	$num_of_pages=ceil(($result->num_rows)/$result_per_page);
+
+	if(!isset($_GET['pageindex'])){
+		$page=1;
+	}else{
+		$page=$_GET['pageindex'];
+	}
+	$this_page_first_result=($page-1)*$result_per_page;
+
+	$sql = $sql." LIMIT ".$this_page_first_result. ",5";
+	$result = $conn->query($sql);
+#write while loop
+	
+	
+	
+	
+	
+	
+	if ($result->num_rows > 0) {
+		
+		while($row = $result->fetch_assoc()) {
+			$rating = 0;
+			switch ($row["security_level"])
+{
+case "Low ++":
+  $rating = 1;
+  break;  
+case "Low +":
+  $rating = 2;
+  break;
+case "Low":
+  $rating = 3;
+  break;
+case "Medium":
+  $rating = 4;
+  break;
+case "High":
+  $rating = 5;
+  break;
+case "High +":
+  $rating = 6;
+  break;
+case "High ++":
+  $rating = 7;
+  break;
+}
+			$starts = str_repeat("★",$rating);
+			echo '<div style="background-color:transparent;">
+<div class="block-grid" style="min-width: 320px; max-width: 600px; overflow-wrap: break-word; word-wrap: break-word; word-break: break-word; Margin: 0 auto; background-color: transparent;">
+<div style="border-collapse: collapse;display: table;width: 100%;background-color:transparent;">
+<div class="col num12" style="min-width: 320px; max-width: 600px; display: table-cell; vertical-align: top; width: 600px;">
+<div class="col_cont" style="width:100% !important;">
+
+<div style="border-top:0px solid transparent; border-left:0px solid transparent; border-bottom:0px solid transparent; border-right:0px solid transparent; padding-top:5px; padding-bottom:5px; padding-right: 0px; padding-left: 0px;">
+<div style="color:#ffae01;font-family:Montserrat, Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Tahoma, sans-serif;line-height:1.2;padding-top:10px;padding-right:10px;padding-bottom:10px;padding-left:10px;">
+<div class="txtTinyMce-wrapper" style="line-height: 1.2; font-size: 12px; color: #ffae01; font-family: Montserrat, Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Tahoma, sans-serif; mso-line-height-alt: 14px;">
+<p dir="ltr" style="font-size: 22px; line-height: 1.2; word-break: break-word; text-align: left; mso-line-height-alt: 26px; margin: 0;"><span style="font-size: 22px;"><strong>'.'____________________________________________________'.$row["sensor_desc"]."</strong>". '</span></p>
+</div>
+</div>
+
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+</div>
+<div style="background-color:transparent;">
+<div class="block-grid mixed-two-up" style="min-width: 320px; max-width: 600px; overflow-wrap: break-word; word-wrap: break-word; word-break: break-word; Margin: 0 auto; background-color: transparent;">
+<div style="border-collapse: collapse;display: table;width: 100%;background-color:transparent;">
+<div class="col num8" style="display: table-cell; vertical-align: top; max-width: 320px; min-width: 400px; width: 400px;">
+<div class="col_cont" style="width:100% !important;">
+
+<div style="border-top:0px solid transparent; border-left:0px solid transparent; border-bottom:0px solid transparent; border-right:0px solid transparent; padding-top:5px; padding-bottom:5px; padding-right: 0px; padding-left: 0px;">
+<div style="color:#393d47;font-family:Montserrat, Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Tahoma, sans-serif;line-height:1.2;padding-top:10px;padding-right:10px;padding-bottom:10px;padding-left:10px;">
+<div class="txtTinyMce-wrapper" style="font-size: 12px; line-height: 1.2; color: #393d47; font-family: Montserrat, Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Tahoma, sans-serif; mso-line-height-alt: 14px;">
+
+<p style="line-height: 1.2; word-break: break-word; mso-line-height-alt: 14px; margin: 0;">Sensor Location : ' . $row["sensor_name"]. '</p>
+<p style="line-height: 1.2; word-break: break-word; mso-line-height-alt: 14px; margin: 0;">Security Level : '. $starts.'</p>
+</div>
+</div>
+
+</div>
+
+</div>
+</div>
+<div class="col num4" style="display: table-cell; vertical-align: top; max-width: 320px; min-width: 200px; width: 200px;">
+<div class="col_cont" style="width:100% !important;">
+
+<div style="border-top:0px solid transparent; border-left:0px solid transparent; border-bottom:0px solid transparent; border-right:0px solid transparent; padding-top:5px; padding-bottom:5px; padding-right: 0px; padding-left: 0px;">
+
+<div align="center" class="button-container" style="padding-top:10px;padding-right:10px;padding-bottom:10px;padding-left:10px;">
+<a href="https://www.melwomensafety.net/display-map/?latitude='.$row["latitude"].'&longitude='.$row["longitude"].'" style="-webkit-text-size-adjust: none; text-decoration: none; display: inline-block; color: #ffffff; background-color: #ffae01; border-radius: 0px; -webkit-border-radius: 0px; -moz-border-radius: 0px; width: auto; width: auto; border-top: 0px solid #8a3b8f; border-right: 0px solid #8a3b8f; border-bottom: 0px solid #8a3b8f; border-left: 0px solid #8a3b8f; padding-top: 5px; padding-bottom: 5px; font-family: Montserrat, Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Tahoma, sans-serif; text-align: center; mso-border-alt: none; word-break: keep-all;" target="_blank"><span style="padding-left:20px;padding-right:20px;font-size:16px;display:inline-block;letter-spacing:undefined;"><span style="font-size: 16px; line-height: 2; word-break: break-word; mso-line-height-alt: 32px;">Go To ></span></span></a>
+
+</div>
+
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+</div>'. "<br>";
+		}
+	} else {
+		echo "0 result";
+	}
+
+
+
+
+	for($page=1;$page<=$num_of_pages;$page++){
+		if($search){
+			echo '<a href="https://www.melwomensafety.net/map/?search='.$search.'&pageindex='.$page.'">'.$page.'</a>'." ";
+		}else{
+			echo '<a href="https://www.melwomensafety.net/map/?pageindex='.$page.'">'.$page.'</a>'." ";
+		}
+		
+	}	
+	
+	
+	
+?>
+
+
+</div>
+</td>
+</tr>
+</tbody>
+</table>
+
+</body>
+</html>
